e93bdd695ab5867b7a10a7cd179861a986a40433
rifqimunawar <rifqimunawar48@gmail.com>
2023-05-17 16:11:44 +0700

    upload ss output

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,6 +11,46 @@ use Illuminate\Support\Facades\Validator;
 
 class LoginController extends Controller
 {
+    // request register akun 
+    public function register()
+    {
+        return view('auth.register');
+    }
+    public function store(Request $request)
+    {
+        // Rule validasi untuk username dan password
+        $rules = [
+            'username' => 'required|unique:users,username',
+            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
+        ];
+        // Pesan validasi
+        $messages = [
+            'username.required' => 'Username wajib diisi.',
+            'username.unique' => 'Username sudah digunakan.',
+            'password.required' => 'Password wajib diisi.',
+            'password.min' => 'Password minimal 8 karakter.',
+            'password.regex' => 'Password harus terdiri dari 
+            huruf kapital, huruf kecil, dan angka.',
+        ];
+        // Validasi input
+        $validator = Validator::make($request->all(), $rules, $messages);
+    
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+    
+        // Buat user baru
+        $user = User::create([
+            'name'=>$request->name,
+            'username'=>$request->username,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+        ]);
+    
+        Alert::success('Terima Kasih', 'Anda Berhasil Register');
+        return redirect()->to('/login');
+    }
+
   // ketika user mengakses /login maka akan di berikan form login
   public function login()
   {
@@ -33,60 +73,13 @@ class LoginController extends Controller
             // Authentication failed...
             $user = User::where('username', $credentials['username'])->first();
             if (!$user) {
-                return redirect()->back()->with('error', 'Username atau Password Salah');
+                return redirect()->back()->with(
+                  'error', 'Username atau Password Salah');
             } else {
-                return redirect()->back()->with('error', 'Username atau Password Salah');
+                return redirect()->back()->with(
+                  'error', 'Username atau Password Salah');
             }
         }
-    }
-
-    // request register akun 
-    public function register()
-    {
-        return view('auth.register');
-    }
-    public function store(Request $request)
-    {
-        // Rule validasi untuk username dan password
-        $rules = [
-            'username' => 'required|unique:users,username',
-            'password' => 'required|min:8|regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/',
-        ];
-    
-        // Pesan validasi
-        $messages = [
-            'username.required' => 'Username wajib diisi.',
-            'username.unique' => 'Username sudah digunakan.',
-            'password.required' => 'Password wajib diisi.',
-            'password.min' => 'Password minimal 8 karakter.',
-            'password.regex' => 'Password harus terdiri dari huruf kapital, huruf kecil, dan angka.',
-        ];
-    
-        // Validasi input
-        $validator = Validator::make($request->all(), $rules, $messages);
-    
-        if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
-        }
-    
-        // Buat user baru
-        $user = User::create([
-            'name'=>$request->name,
-            'username'=>$request->username,
-            'email'=>$request->email,
-            'password'=>bcrypt($request->password),
-        ]);
-    
-        Alert::success('Terima Kasih', 'Anda Berhasil Register');
-        return redirect()->to('/login');
-    }
-
-    public function logout(Request $request): RedirectResponse
-    {
-        Auth::logout();
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
-        return redirect ('/');
     }
 
     public function dashboard(Request $request)
@@ -94,5 +87,13 @@ class LoginController extends Controller
       $user = Auth::user();
 
       return view('dashboard', compact('user'));
+    }
+    
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect ('/');
     }
 }
